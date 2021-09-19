dd23f48171b82d5af9e99d5d19477f141c04d96d
andreclerigo Desktop <andreclerigo@outlook.com>
2021-09-19 14:32:48 +0100

    kuantokusta getting all the data from the 1st page

--- scraper.py
+++ scraper.py
@@ -1,8 +1,8 @@
 import re
+from time import process_time
 import requests
 import pandas as pd
 import urllib.parse
-from time import sleep
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -10,11 +10,15 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-names = []  # List to store the names of the products
-prices = [] # List to store the prices of the products
-links=[]    # List to store the links of the products
+# Lists to store second hand names, prices and links of the products
+sh_names = [] 
+sh_prices = [] 
+sh_links=[]    
 
-
+# Lists to store first hand names, prices and links of the products
+fh_names = [] 
+fh_prices = [] 
+fh_links=[]    
 
 
 #OLX search function----------------------------------------------------------------------
@@ -39,9 +43,9 @@ def olx_search(location, search_term):
                 product_link = product.find('a')['href']
                 
                 # Append the data to the lists
-                names.append(product_name)
-                prices.append(product_price)
-                links.append(product_link)
+                sh_names.append(product_name)
+                sh_prices.append(product_price)
+                sh_links.append(product_link)
 
                 counter = counter + 1   # For deubgging purposes
                 print(f'Name: {product_name}\nPrice: {product_price}€\nLink: {product_link}\n')
@@ -50,29 +54,38 @@ def olx_search(location, search_term):
     print(counter)  # For deubgging purposes
 
 
-
 #KuantoKusta search function--------------------------------------------------------------
 def kk_search(search_term):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
     page = requests.get(f'https://www.kuantokusta.pt/search?q={search_term}&sort=3', headers=headers)
     soup = BeautifulSoup(page.text, 'lxml')
 
-    product = soup.find('div', class_='product-item-inner')
-
-    product_name = product.find('a', class_='product-item-name')['title']
-    product_price = product.find('a', class_='product-item-price').find('span').text[:-1].replace(',', '.')
-    product_link = 'https://kuantokusta.pt/' + product.find('a', class_='product-item-store')['href']
-
-    if product_link == 'https://kuantokusta.pt/#':
-        encoded_link = product.find('a', class_='product-item-store')['onclick'].split(',')[2].strip().replace('"', '').replace('\'', '')
-        product_link = urllib.parse.unquote(encoded_link)
+    products = soup.find_all('div', class_='product-item-inner')
     
-    return {'nome': product_name, 'preco': product_price, 'link': product_link}
+    for product in products:
+        # Get the data
+        product_name = product.find('a', class_='product-item-name')['title']
+        product_price = product.find('a', class_='product-item-price').find('span').text[:-1].replace('.', '').replace(',', '.').strip()
+        if 'Desde' in product_price:
+            product_price = product_price[5:].strip()
+        product_price = float(product_price)
 
+        product_link = 'https://kuantokusta.pt/' + product.find('a', class_='product-item-store')['href']
+
+        if product_link == 'https://kuantokusta.pt/#':
+            encoded_link = product.find('a', class_='product-item-store')['onclick'].split(',')[2].replace('"', '').replace('\'', '').strip()
+            product_link = urllib.parse.unquote(encoded_link)
+        
+        # Append the data to the lists
+        fh_names.append(product_name)
+        fh_prices.append(product_price)
+        fh_links.append(product_link)
+
+        print(f'Name: {product_name}\nPrice: {product_price}€\nLink: {product_link}\n')
 
 
 #FacebookMarketplace search function------------------------------------------------------Probabbly illegal, too bad
-# def face_search():
+# def fb_search():
 #     starting_url="https://www.facebook.com"
 #     email="joaosilvascraper@gmail.com"
 #     password="yJ-B'#YsEf.^G75H"
@@ -83,7 +96,6 @@ def kk_search(search_term):
     
 #     driver.close
     
-
 
 def main():
     location_olx = 'ads'
@@ -96,13 +108,16 @@ def main():
     kk_search_term = re.sub('\\s+', '+', search_term)       # Replace white spaces rows with '+'
 
     olx_search(location_olx, olx_search_term)               # Populate the list with OLX data
-    print(kk_search(kk_search_term))
+    kk_search(kk_search_term)
 
-    #face_search()
+    #fb_search()
 
-    d = {'nome': names, 'precos': prices, 'links': links}
-    pd.DataFrame(d).sort_values('precos').to_json('produtos.json', orient='index', indent=2, force_ascii=False)
-    pd.DataFrame(kk_search(kk_search_term), index=[0]).to_json('best.json', orient='index', indent=2, force_ascii=False)
+    sh_d = {'nome': sh_names, 'precos': sh_prices, 'links': sh_links}
+    pd.DataFrame(sh_d).sort_values('precos').to_json('sh_products.json', orient='index', indent=2, force_ascii=False)
+
+    fh_d = {'nome': fh_names, 'precos': fh_prices, 'links': fh_links}
+    pd.DataFrame(fh_d).sort_values('precos').to_json('fh_products.json', orient='index', indent=2, force_ascii=False)
+
 
 if __name__ == '__main__':
     main()
