b492bc466737cb5db4a4f2b268b80eec89c67532
andreclerigo Desktop <andreclerigo@outlook.com>
2021-09-22 00:29:38 +0100

    ebay scrape not working past page 5

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+from posixpath import pardir
 import re
 import os
 from time import process_time
@@ -65,6 +66,7 @@ def cj_search(location, search_term):
         soup = BeautifulSoup(page.text, 'lxml')
         products = soup.find_all('div', class_='container_related')
 
+        # Stop when a blank is hitted
         if not products:
             break
 
@@ -82,7 +84,62 @@ def cj_search(location, search_term):
             counter = counter + 1   # For deubgging purposes
             page_num = page_num + 1
             print(f'Name: {product_name}\nPrice: {product_price}€\nLink: {product_link}\n')
-    print(counter)
+    print(counter)  # For deubgging purposes
+
+
+def ebay_search(search_term):
+    page_num = 5
+    while True:
+        headers = {'user-agent': 'Mozilla/5.0'}
+        page = requests.get(f'https://www.ebay.com/sch/i.html?_nkw={search_term}&_fcid=164&_sop=15&_pgn={page_num}', headers=headers)
+        soup = BeautifulSoup(page.text, 'lxml')
+
+        products = soup.find_all('li', class_='s-item')
+        
+        counter = 0
+        for product in products:
+            try:             
+                product_name = product.find('h3', class_='s-item__title').text
+                # Retrieve the section with price and shipping information
+                product_info = product.find('div', class_='s-item__details').find_all('div', class_='s-item__detail--primary')
+                product_base_price = 0
+                cost_shipping = -1
+
+                # Check for price and shipping cost
+                for info in product_info:                
+                    try:
+                        product_base_price = float(info.find('span', class_='s-item__price').text[4:].replace(',', '.').replace(u'\xa0', u''))
+                    except:
+                        try:
+                            cost_shipping = str(info.find('span', class_='s-item__shipping').text)
+
+                            # If the text says 'grátis' then the cost is 0
+                            if ('grátis' or 'não') in cost_shipping:
+                                cost_shipping = 0
+                            # If the text has '+' then a number is there
+                            if 'EUR' in cost_shipping:
+                                cost_shipping = float(cost_shipping.replace(',', '.')[5:].split()[0])
+                        except:
+                            # If no price or shipping are found then skip to the next div
+                            continue
+                
+                # Remove ads that don't show the shipping price
+                if cost_shipping == -1:
+                    continue
+
+                product_price = round(product_base_price + cost_shipping, 2)
+                product_link = product.find('a', 's-item__link')['href']
+            except:
+                continue
+
+            counter = counter + 1   # For deubgging purposes
+            print(f'Name: {product_name}\nPrice: {product_base_price}€ + {cost_shipping} ({product_price})€\nLink: {product_link}\n')
+
+        page_num = page_num + 1
+        if counter == 0:    
+            break
+        print(page_num)
+    print(page_num - 1)  # For deubgging purposes
 
 
 # FacebookMarketplace search function------------------------------------------------------Probabbly illegal, too bad
@@ -114,12 +171,14 @@ def kk_search(search_term):
         # Get the data
         product_name = product.find('a', class_='product-item-name')['title']
         product_price = product.find('a', class_='product-item-price').find('span').text[:-1].replace('.', '').replace(',', '.').strip()
+        # Done when the price tag has text
         if 'Desde' in product_price:
             product_price = product_price[5:].strip()
         product_price = float(product_price)
 
         product_link = 'https://kuantokusta.pt/' + product.find('a', class_='product-item-store')['href']
 
+        # Done when the link goes directly to the store and not to a KuantoKusta Page
         if product_link == 'https://kuantokusta.pt/#':
             encoded_link = product.find('a', class_='product-item-store')['onclick'].split(',')[2].replace('"', '').replace('\'', '').strip()
             product_link = urllib.parse.unquote(encoded_link)
@@ -135,19 +194,22 @@ def kk_search(search_term):
 def main():
     location_olx = 'ads'
     location_cj = 'portugal'
-    search_term = input("Procurar: ").lower()               # Product name
-    location_input = input('Regiao: ').lower()              # Region name
+    search_term = 'gtx 1080'#input("Procurar: ").lower()               # Product name
+    location_input = ''#input('Regiao: ').lower()              # Region name
     if location_input != '':
         location_olx = location_input                       # Enter means the entier market
 
     olx_search_term = re.sub('\\s+', '-', search_term)      # Replace white spaces rows with '-'
     kk_search_term = re.sub('\\s+', '+', search_term)       # Replace white spaces rows with '+'
     cj_search_term = kk_search_term
+    ebay_search_term = kk_search_term
 
-    olx_search(location_olx, olx_search_term)               # Populate the list with OLX data
-    cj_search(location_cj, cj_search_term)                  # Populate the list wtih CustoJusto data
+    #olx_search(location_olx, olx_search_term)               # Populate the list with OLX data
+    #cj_search(location_cj, cj_search_term)                  # Populate the list wtih CustoJusto data
 
-    kk_search(kk_search_term)
+    #kk_search(kk_search_term)
+
+    ebay_search(ebay_search_term)
 
     #fb_search()
 
