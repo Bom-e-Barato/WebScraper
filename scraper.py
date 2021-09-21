8b5efd860b3fdb8d8554289c8cb5d7a9c01d44c8
andreclerigo Desktop <andreclerigo@outlook.com>
2021-09-22 00:46:52 +0100

    added function to automate list append and add site parameter

--- scraper.py
+++ scraper.py
@@ -18,19 +18,25 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 
 # Lists to store second hand names, prices and links of the products
-sh_names = [] 
-sh_prices = [] 
-sh_links=[]    
+sh_names = []
+sh_prices = []
+sh_links = []
+sh_sites = []
 
 # Lists to store first hand names, prices and links of the products
-fh_names = [] 
-fh_prices = [] 
-fh_links=[]    
+fh_names = []
+fh_prices = []
+fh_links = []
 
 
-# OLX search function----------------------------------------------------------------------
+def sh_append(name, price, link, site):
+    sh_names.append(name)
+    sh_prices.append(price)
+    sh_links.append(link)
+    sh_sites.append(site)
+
+
 def olx_search(location, search_term):
-    counter = 0
     for i in range(25):
         page = requests.get(f'https://www.olx.pt/{location}/q-{search_term}/?page={i+1}')
         soup = BeautifulSoup(page.text, 'lxml')
@@ -50,15 +56,11 @@ def olx_search(location, search_term):
                 product_link = product.find('a')['href']
                 
                 # Append the data to the lists
-                sh_names.append(product_name)
-                sh_prices.append(product_price)
-                sh_links.append(product_link)
+                sh_append(name=product_name, price=product_price, link=product_link, site='olx')
 
-                counter = counter + 1   # For deubgging purposes
                 print(f'Name: {product_name}\nPrice: {product_price}€\nLink: {product_link}\n')
             except:
                 continue
-    print(counter)  # For deubgging purposes
 
 
 def cj_search(location, search_term):
@@ -74,15 +76,16 @@ def cj_search(location, search_term):
             break
 
         for product in products:
-            # Get the data
-            product_name = product.find('h2', class_='title_related').find('b').text
-            product_price = float(product.find('h5', class_='price_related').text.strip()[:-2])
-            product_link = product.find('a')['href']
+            try:
+                # Get the data
+                product_name = product.find('h2', class_='title_related').find('b').text
+                product_price = float(product.find('h5', class_='price_related').text.strip()[:-2])
+                product_link = product.find('a')['href']
 
-            # Append the data to the lists
-            sh_names.append(product_name)
-            sh_prices.append(product_price)
-            sh_links.append(product_link)
+                # Append the data to the lists
+                sh_append(name=product_name, price=product_price, link=product_link, site='custojusto')
+            except:
+                continue
 
             counter = counter + 1   # For deubgging purposes
             page_num = page_num + 1
@@ -91,7 +94,7 @@ def cj_search(location, search_term):
 
 
 def ebay_search(search_term):
-    page_num = 5
+    page_num = 1
     while True:
         headers = {'user-agent': 'Mozilla/5.0'}
         page = requests.get(f'https://www.ebay.com/sch/i.html?_nkw={search_term}&_fcid=164&_sop=15&_pgn={page_num}', headers=headers)
@@ -134,10 +137,13 @@ def ebay_search(search_term):
                 product_link = product.find('a', 's-item__link')['href']
             except:
                 continue
+            
+            # Append the data to the lists
+            sh_append(name=product_name, price=product_price, link=product_link, site='ebay')
 
             counter = counter + 1   # For deubgging purposes
             print(f'Name: {product_name}\nPrice: {product_base_price}€ + {cost_shipping} ({product_price})€\nLink: {product_link}\n')
-
+            
         page_num = page_num + 1
         if counter == 0:    
             break
@@ -181,8 +187,6 @@ def fb_search(search_term):
     #driver.close
 
 
-
-# KuantoKusta search function--------------------------------------------------------------
 def kk_search(search_term):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
     page = requests.get(f'https://www.kuantokusta.pt/search?q={search_term}&sort=3', headers=headers)
@@ -227,14 +231,14 @@ def main():
     cj_search_term = kk_search_term
     ebay_search_term = kk_search_term
 
-    #olx_search(location_olx, olx_search_term)               # Populate the list with OLX data
-    #cj_search(location_cj, cj_search_term)                  # Populate the list wtih CustoJusto data
+    olx_search(location_olx, olx_search_term)               # Populate the list with OLX data
+    cj_search(location_cj, cj_search_term)                  # Populate the list wtih CustoJusto data
     ebay_search(ebay_search_term)
-    fb_search(search_term)
+    #fb_search(search_term)
 
-    #kk_search(kk_search_term)
+    kk_search(kk_search_term)
 
-    sh_d = {'nome': sh_names, 'precos': sh_prices, 'links': sh_links}
+    sh_d = {'nome': sh_names, 'precos': sh_prices, 'links': sh_links, 'sites': sh_sites}
     pd.DataFrame(sh_d).sort_values('precos').to_json('sh_products.json', orient='index', indent=2, force_ascii=False)
 
     fh_d = {'nome': fh_names, 'precos': fh_prices, 'links': fh_links}
