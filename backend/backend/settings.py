22d1403ca31a40aef56d47fc3e13b3988b0d4fe1
AndreClerigo Laptop <andreclerigo@outlook.com>
2022-06-20 08:45:38 +0100

    allow all hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-9p_plh1qgv(j!a)d)*w=x8k&yerclje#y)@$+t5!@lxp80pbt2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
